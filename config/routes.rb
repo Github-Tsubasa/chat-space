f2647f62134697c77ff47b547873c52e5d1be917
Github-Tsubasa <59303334+Github-Tsubasa@users.noreply.github.com>
2020-01-24 21:04:22 +0900

    commit9

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:edit, :update]
   # resources :groups, only: [:new, :create]
-  resources :groups, only: [:index, :create, :edit, :update] do
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
 
