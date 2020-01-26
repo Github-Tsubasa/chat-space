d5ccefde29f4a734b26cba182c7fc82fef4e24f1
Github-Tsubasa <59303334+Github-Tsubasa@users.noreply.github.com>
2020-01-26 15:51:04 +0900

    commit1

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,9 @@ Rails.application.routes.draw do
   # get 'messages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   # resources :groups, only: [:new, :create]
-  resources :groups, only: [:index, :create, :edit, :update]
+  resources :groups, only: [:index, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
-
-    def show_last_message
-      if (last_message = messages.last).present?
-        if last_message.content?
-          last_message.content
-        else
-          '画像が投稿されています'
-        end
-      else
-        'まだメッセージはありません。'
-      end
-    end
   end
+end
