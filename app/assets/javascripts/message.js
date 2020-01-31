0d9dc1d62dae590932f414ab3a3934f42ca0cc48
Github-Tsubasa <59303334+Github-Tsubasa@users.noreply.github.com>
2020-01-31 10:08:34 +0900

    commit2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,12 @@
 $(function(){
+  console.log(last_message_id);
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
+
+    last_message_id = $('.message:last').data("message-id");
     $.ajax({
       url: url,
       type: "POST",
@@ -12,10 +16,12 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);      
-      $('form')[0].reset();
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      if (messages.length !== 0) {
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
@@ -24,6 +30,7 @@ $(function(){
       $('.messages').prop('disabled', false);
     });
   });
+  setInterval(reloadMessages, 7000);
 });
 
 
